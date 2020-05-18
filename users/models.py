79f28c9f876e51a7c2c1bef73427f1bd6902a578
Hiroki <huchida0622@gmail.com>
2020-05-18 16:38:14 +0900

    reset image

--- users/models.py
+++ users/models.py
@@ -1,12 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import User
-# from PIL import Image
+from PIL import Image
 
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     abc = models.CharField(max_length=200)
-    # image = models.ImageField(default='default.png', upload_to='profile_pics')
+    image = models.ImageField(default='default.png', upload_to='profile_pics')
 
     def __str__(self):
         return f'{self.user.username} Profile'
@@ -14,9 +14,9 @@ class Profile(models.Model):
     def save(self, *args, **kwargs):
         super().save(*args, **kwargs)
 
-        # img = Image.open(self.image.path)
+        img = Image.open(self.image.path)
 
-        # if img.height > 300 or img.width > 300:
-        #     output_size = (300, 300)
-        #     img.thumbnail(output_size)
-        #     img.save(self.image.path)
+        if img.height > 300 or img.width > 300:
+            output_size = (300, 300)
+            img.thumbnail(output_size)
+            img.save(self.image.path)
